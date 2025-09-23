d56454e6931dfc255e50d278f0b63ecd871c60e5
Vladimir Borovikov <91381325+iamvldmrbrvkv@users.noreply.github.com>
2025-09-23 20:48:02 +0300

    Remove unnecessary Router wrapper from App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { DarkModeProvider } from './context';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import ContactPage from './pages/Contact';
+import { Routes, Route } from "react-router-dom";
+import { DarkModeProvider } from "./context";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import ContactPage from "./pages/Contact";
 
 function App() {
   return (
     <DarkModeProvider>
-      <Router>
-        <div className="min-h-screen bg-white dark:bg-gray-800 text-black dark:text-gray-100 font-mono">
-          <Header />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/contact" element={<ContactPage />} />
-          </Routes>
-          <Footer />
-        </div>
-      </Router>
+      <div className="min-h-screen bg-white dark:bg-gray-800 text-black dark:text-gray-100 font-mono">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<ContactPage />} />
+        </Routes>
+        <Footer />
+      </div>
     </DarkModeProvider>
   );
 }
