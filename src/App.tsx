b22e9f6fbd60283315f9590962ca8cfec84bdbe6
Vladimir Borovikov <91381325+iamvldmrbrvkv@users.noreply.github.com>
2025-09-23 22:06:05 +0300

    Wrap App component in Router for improved routing structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { DarkModeProvider } from "./context";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -8,14 +8,16 @@ import ContactPage from "./pages/Contact";
 function App() {
   return (
     <DarkModeProvider>
-      <div className="min-h-screen bg-white dark:bg-gray-800 text-black dark:text-gray-100 font-mono">
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/contact" element={<ContactPage />} />
-        </Routes>
-        <Footer />
-      </div>
+      <Router>
+        <div className="min-h-screen bg-white dark:bg-gray-800 text-black dark:text-gray-100 font-mono">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/contact" element={<ContactPage />} />
+          </Routes>
+          <Footer />
+        </div>
+      </Router>
     </DarkModeProvider>
   );
 }
